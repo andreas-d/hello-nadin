cdb185645122c714e111bd3f97a5769e5f7bc845
Andreas Deutinger <andreas.deutinger@porscheinformatik.at>
2017-03-29 14:05:24 +0200

    init project commit

---
+++ src/main/java/poi/nadin/HelloWorld.java
@@ -0,0 +1,16 @@
+package poi.nadin;
+
+/**
+ * Created by yda on 29.03.2017.
+ */
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your Message : " + message);
+    }
+}
